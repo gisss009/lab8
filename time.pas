b82719a2b3e325739418143ba302edfe1e73d62c
Виктория Горбанева <viktoryagorbaneva@yandex.ru>
2023-09-30 09:26:11 +0300

    Количество дней в году

--- time.pas
+++ time.pas
@@ -1,6 +1,6 @@
 ﻿begin
   var yr := Readinteger('Введите год');
   if (yr mod 4 <> 0) or ((yr mod 400 <> 0) and (yr mod 100 = 0)) then
-    print('Не високосный год')
-  else print('Год високосный')
+    print('365')
+  else print('366)
 end.
