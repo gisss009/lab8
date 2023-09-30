f7884532fa2250cec8955e86895ae108817c8e02
Виктория Горбанева <viktoryagorbaneva@yandex.ru>
2023-09-30 09:23:43 +0300

    Проверка года на високосность

---
+++ time.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var yr := Readinteger('Введите год');
+  if (yr mod 4 <> 0) or ((yr mod 400 <> 0) and (yr mod 100 = 0)) then
+    print('Не високосный год')
+  else print('Год високосный')
+end.
