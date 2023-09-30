848214af8cbb1bd313680cc815dfbb76d34f0108
Виктория Горбанева <viktoryagorbaneva@yandex.ru>
2023-09-30 09:10:40 +0300

    Ваше сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
 ﻿begin
-  print('Hello world!')
+  var a := ReadInteger('Который сейчас час?');
+  Assert(a in 0..23);
+  if a in 4..10 then
+    print('Доброе утро, мир!');
+  if a in 11..16 then
+    print('Добрый день, мир!');
+  if a in 17..23 then
+    print('Добрый ночи, мир!');
 end.
